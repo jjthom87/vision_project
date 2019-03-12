efb4f18af7dd33b6a6d81be8e294c1a4afeb786d
Jared Thomas <jjthom87@yahoo.com>
2019-03-12 19:28:31 -0400

    adding

--- vision.js
+++ vision.js
@@ -1,4 +1,8 @@
 // Read the file into memory.
+//https://cloud.google.com/docs/authentication/api-keys?hl=en&visit_id=636878419186004464-2679101247&rd=1#creating_an_api_key
+//https://cloud.google.com/vision/docs/using-curl
+//https://console.cloud.google.com/home/dashboard?project=monastir-167517
+//https://cloud.google.com/vision/docs/base64
 var fs = require('fs');
 var request = require('request');
 
